6025d4f19cb0623b45ad5cc05d2080b7cc492c5f
giampaolli <giampaoli2011@gmail.com>
2019-02-14 14:39:27 -0200

    s3 integration

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
+require("dotenv").config();
 const express = require("express");
-const morgan = require("morgan")
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const path = require("path");
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended :true }));
-app.use(morgan('dev'));
+/**
+ * Database setup
+ */
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true
+});
 
-app.use(require('./routes'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(
+  "/files",
+  express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
+);
+
+app.use(require("./routes"));
 
 app.listen(3000, () => {
-    console.log('Listen on port 3000')
+  console.log("Listen on port 3000");
 });
